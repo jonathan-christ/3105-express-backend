d5c4fcd0c87e2f1f2d94d1580c54b716bdb7fbbf
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-10-04 22:52:43 +0800

    chore(env.ts): added default values for rate limit constants

--- config/env.ts
+++ config/env.ts
@@ -1,6 +1,6 @@
 export const env = {
-  JWT_SECRET: process.env.JWT_SECRET || 'CIS_3105-ExPrEssBacKeNd',
+  JWT_SECRET: process.env.JWT_SECRET || 'CS_3105-ExPrEssBacKeNd',
   PORT: process.env.PORT || 8080,
-  RATE_LIMIT_MAX_REQ: 5,   // max requests per interval (default: 5)
-  RATE_LIMIT_WINDOW_MS: 30*1000  //window duration in ms (default: 30s)
+  RATE_LIMIT_MAX_REQ: Number(process.env.MAX_REQUESTS) || 5,   // max requests per interval (default: 5)
+  RATE_LIMIT_WINDOW_MS: Number(process.env.RATE_LIMIT_WINDOW_MS) || 30 * 1000  //window duration in ms (default: 30s)
 }
