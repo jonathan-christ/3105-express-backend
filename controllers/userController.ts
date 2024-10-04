4ed2e52abe684c34f9fd09da11c1b589edfe97f7
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-10-04 14:43:11 +0800

    feat(userController): added userController
    - added login method
    - added register method
    - added getProfile method to get user's own profile data
    - implemented joi for input validation
    - implemented jwt for token-based auth

---
+++ controllers/userController.ts
@@ -0,0 +1,68 @@
+import type { Request, Response } from "express";
+import { UserModel } from "../models/usersModel.ts";
+import { env } from "../config/env.ts";
+import Joi from "joi";
+import jwt from "jsonwebtoken";
+
+class UserController extends UserModel {
+  private readonly loginSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  });
+
+  private readonly registerSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    }).required(),
+  });
+
+
+  public login = (req: Request, res: Response): void => {
+    const { error, value } = this.loginSchema.validate(req.body);
+
+    if (error) {
+      res.status(400).json({ message: error.message });
+    }
+
+    const user = this.getUserByDetails(value.username, value.password);
+
+    if (!user) {
+      res.status(400).json({ message: "Invalid credentials." });
+      return;
+    }
+
+    const token = jwt.sign({
+      id: user.id,
+      username: user.username,
+      email: user.email
+    }, env.JWT_SECRET, {
+      expiresIn: '1h'
+    });
+
+    res.status(200).json({ message: 'Login successful!', token });
+  };
+
+  public register = (req: Request, res: Response): void => {
+    const { error, value } = this.registerSchema.validate(req.body);
+
+    if (error) {
+      res.status(400).json({ message: error.message });
+    }
+
+    const user = this.createUser(value.username, value.password, value.email);
+
+    if (!user) {
+      res.status(400).json({ message: "Registration failed!" });
+      return;
+    }
+
+    res.status(200).json({ message: "Registration successful!" });
+  }
+
+  public getProfile = (_: Request, res: Response) => {
+    const user = this.getUserById(res.locals.decodedToken as User)
+  }
+}
